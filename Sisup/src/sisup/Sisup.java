d41bd929e656725563ec1f7f02bf852e98e1efe6
AnthonyCova <anthony_cova@live.com>
2014-01-28 20:07:05 -0800

    cambie el main

--- Sisup/src/sisup/Sisup.java
+++ Sisup/src/sisup/Sisup.java
@@ -14,6 +14,7 @@ public class Sisup {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println("Hola");
         // TODO code application logic here
     }
 }
